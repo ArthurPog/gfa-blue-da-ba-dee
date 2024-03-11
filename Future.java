6bb5d34d0d834681506b17ff74db5fa3264b5694
Shimmyland <simonlibiger@gmail.com>
2024-03-11 15:53:46 +0100

    d5

--- Future.java
+++ Future.java
@@ -2,26 +2,7 @@ import java.util.Scanner;
 
 public class ShimmyFuture {
     public static void getYourHoroscopeSign() {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Your birth month: ");
-        int month = scanner.nextInt();
-        System.out.print("Your birth day: ");
-        int day = scanner.nextInt();
-        scanner.close();
-
-        String[] zodiacSigns = {
-                "Capricorn", "Aquarius", "Pisces", "Aries", "Taurus", "Gemini",
-                "Cancer", "Leo", "Virgo", "Libra", "Scorpio", "Sagittarius"
-        };
-
-        int[] startDates = {20, 19, 21, 20, 21, 21, 23, 23, 23, 23, 22, 22};
-
-        int index = (month - 1 + (day >= startDates[month - 1] ? 1 : 0)) % 12;
-        System.out.println("Your horoscope sign is: " + zodiacSigns[index]);
-        
-        // testing new push
-        // new push
-        
-        // something
+        System.out.println("7");
+	System.out.println("17");
     }
 }
